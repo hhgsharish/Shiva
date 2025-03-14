f3f74234778d034555e0ea2ca1f05b9cc420d879
Harisha G <hhgsharish@gmail.com>
2025-03-14 15:16:23 +0530

    Create backend.tf

---
+++ Terraform/Assignment 5/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "${terraform.workspace}-tf-bucketzhzast21" # change this
+    key            = "state/${terraform.workspace}/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
